d135605aaa53c731575705b5a9476513d9c44c57
Dylan <dylpark@protonmail.com>
2021-03-29 20:10:47 +1000

    *Adding View Binding + Toolbar Colour

--- app/src/main/java/com/example/a7minuteworkout/ExerciseActivity.kt
+++ app/src/main/java/com/example/a7minuteworkout/ExerciseActivity.kt
@@ -1,16 +1,36 @@
 package com.example.a7minuteworkout
 
 import android.os.Bundle
-import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import com.example.a7minuteworkout.databinding.ActivityExerciseBinding
+
+private val toolbar_exercise_activity: Toolbar?
+    get() {
+        TODO("Not yet implemented")
+    }
 
 class ExerciseActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityExerciseBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_exercise)
+        binding = ActivityExerciseBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_exercise)
+
+        setSupportActionBar(toolbar_exercise_activity)
+        val actionbar = supportActionBar
+        if(actionbar != null){
+            actionbar.setDisplayHomeAsUpEnabled(true)
+        }
+        toolbar_exercise_activity?.setNavigationOnClickListener {
+            onBackPressed()
+        }
     }
 
 }
